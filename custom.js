6c96ae0fa6d30786d31f97307c05e5888ef83681
boiler-notes <96404876+boiler-notes@users.noreply.github.com>
2021-12-31 22:55:41 -0600

    Update custom.js

--- custom.js
+++ custom.js
@@ -67,7 +67,7 @@ function changeSpacing(item){
 
 
 function printDiv(item) {
-    var content= document.getElementById("notes");
+    var content= document.getElementById("notes").innerHTML;
     var a = window.open('', '', 'height=500, width=800');
     a.document.write(content);
     a.document.close();
